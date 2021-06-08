7128d26652156f323aeaf734a30a2670f213dcd8
Lisandro <salvareschilisandro0@gmail.com>
2021-06-08 17:19:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+
+app.listen(3001,
+    () => {
+        console.log('el servidor esta escuchando el puerto 3001')
+    }
+)
+
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
