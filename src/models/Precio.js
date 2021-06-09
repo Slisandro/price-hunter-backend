7910d792eb814797a0d98c34991f49b45ce3e9f7
PMolina00 <pmolina9876@gmail.com>
2021-06-09 17:27:27 -0300

    creacion modelos modelo precios, modelo moneda

---
+++ src/models/Precio.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('price', {
+        latitud: { // latitud es obligatorio
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        longitud: { // longitud es obligatorio
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // fecha_hora: { // se crea con el .create
+        //     type: DataTypes.DATE,
+        // },
+        nombre_negocio: { // no es obligatorio
+            type: DataTypes.STRING,
+        },
+        direccion_negocio: { // no es obligatorio
+            type: DataTypes.STRING,
+        },
+        precio: { // valor permitido es unicamente números.
+            type: DataTypes.INTEGER,
+        },
+        // id_desafio: { // ?
+        //     type: DataTypes.STRING,
+        // },
+        // id_usuario: { // ?
+        //     type: DataTypes.STRING,
+        // }
+    });
+};
