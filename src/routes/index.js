076007590ed106f06ecf1e03eb2c3bd00d6b008b
PMolina00 <pmolina9876@gmail.com>
2021-06-10 23:02:13 -0300

    estuctrura basica del post

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const productos = require("./productos")
+const usuarios = require('./usuarios');
 
 const router = Router();
 
 router.use('/', productos)
+router.use('/', usuarios)
 
 
 module.exports = router;
