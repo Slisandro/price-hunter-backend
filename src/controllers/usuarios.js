d3f57f8fe60ab16bc8068664e1f24e7f2daf39db
PMolina00 <pmolina9876@gmail.com>
2021-06-11 14:51:07 -0300

    pre-pull

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,12 +1,25 @@
 const { Usuarios } = require('../db');
-// const axios = require("axios");
-// const { Op } = require("sequelize");
+const bcrypt = require('bcrypt')
 
 async function addUsuarios(req, res, next) {
     const usuario = req.body;
-    console.log(usuario)
+    let password = await bcrypt.hash(usuario.password, 10);
+    console.log(password)
     try {
-        const nuevoUsuario = await Usuarios.create(usuario);
+
+        const nuevoUsuario = await Usuarios.create({
+            nombre: usuario.nombre,
+            apellido: usuario.apellido,
+            fecha_de_nacimiento: usuario.fecha_de_nacimiento,
+            ciudadId: usuario.ciudadId,
+            generoId: usuario.generoId, 
+            metodo_de_cobro: usuario.metodo_de_cobro, 
+            banco: usuario.banco,
+            numero_de_cuenta: usuario.numero_de_cuenta,
+            tipoUsuarioId: usuario.tipoUsuarioId,
+            password: password,
+        });
+
         return res.send(nuevoUsuario);
     } catch (err) {
         next(err);
