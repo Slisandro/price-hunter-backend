3ec86c7d19a957528bcc310225454e5989886ca3
nicoarguello <nicoarguello36@gmail.com>
2021-06-11 05:07:03 -0300

    Ruta de busqueda por subcategorias

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const productos = require("./productos")
+const subcategoria = require('./subcategoria.js')
 
 const router = Router();
 
 router.use('/', productos)
+router.use('/', subcategoria)
 
 
 module.exports = router;
