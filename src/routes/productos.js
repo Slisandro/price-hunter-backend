351ca1f37c0f0472bcc4527044eb606804273ae8
PMolina00 <pmolina9876@gmail.com>
2021-06-13 15:18:22 -0300

    comentarios y arreglos de auth

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -2,11 +2,11 @@
 const { productos } = require('../controllers/productos');
 const { Router } = require('express');
 const router = Router();
-const middlerware = require('../midelwares/auth')
+// const middlerware = require('../midelwares/auth')
 //agregar middlerware para la restricción, para que este logeado 
-router.get('/productos', middlerware, productos) 
+// router.get('/productos', middlerware, productos) 
 
-// router.use('/productos', productos)
+router.use('/productos', productos)
 
 
 module.exports = router;
