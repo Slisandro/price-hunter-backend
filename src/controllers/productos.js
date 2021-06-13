02d90ba77a798c61166b538b174c2829fade3f35
PMolina00 <pmolina9876@gmail.com>
2021-06-13 14:45:35 -0300

    pre pull

--- src/controllers/productos.js
+++ src/controllers/productos.js
@@ -1,56 +1,66 @@
-const {Productos, Precio, Desafios} = require('../db'); //fijarnos el nombre con que lo pone pablo.
-const axios = require ("axios");
+const { Productos, Precio, Desafios } = require('../db'); //fijarnos el nombre con que lo pone pablo.
+const axios = require("axios");
 const { Op } = require("sequelize");
 
 
 
-function productos(req,res,next){
-    
-    const nombre_input = req.query.name;  
-    if(nombre_input!==undefined){  //condicion del front mandar id_subcategoria y/o mandar nombre_producto.
+function productos(req, res, next) {
+    Precio.findAll({
+        where: { usuarioId: 1 },//aca el id del user / req.user.id
+    }).then(precios => {
+        console.log(precios.map(price => {
+            return price.dataValues
+        })
+        )
+    }
+    )
+
+
+    const nombre_input = req.query.name;
+    if (nombre_input !== undefined) {  //condicion del front mandar id_subcategoria y/o mandar nombre_producto.
 
         const nombre_min = nombre_input.toLowerCase();
         Productos.findAll({
-            where:{
-                nombre:{
+            where: {
+                nombre: {
                     [Op.substring]: `${nombre_min}`
                 }
             },
-            attributes:["nombre"], //cont_neto , unidad_medida, id_sub_categoria
-            include:  
+            attributes: ["nombre"], //cont_neto , unidad_medida, id_sub_categoria
+            include:
             {
                 model: Desafios,
-                attributes:['nombre_desafio'],
+                attributes: ['nombre_desafio'],
                 include:
                 {
                     model: Precio,
-                    attributes:['precio'], //fecha_captura  
+                    attributes: ['precio'], //fecha_captura  
                 }
             }
         })
-        // .then((respuesta)=>{res.send(respuesta[0].desafios[0].precios)})
-        .then((respuesta)=>{
-            
-            const array_productos = [];
+            // .then((respuesta)=>{res.send(respuesta[0].desafios[0].precios)})
+            .then((respuesta) => {
 
-            respuesta.forEach(producto => {
-                producto.desafios.forEach((desafio)=>{
-                    desafio.precios.forEach((precioo)=>{
-                        const obj = {
-                            precio: precioo.precio,
-                            desafio: desafio.nombre_desafio,
-                            preoducto: producto.nombre
-                        }
-                        array_productos.push(obj);
+                const array_productos = [];
+
+                respuesta.forEach(producto => {
+                    producto.desafios.forEach((desafio) => {
+                        desafio.precios.forEach((precioo) => {
+                            const obj = {
+                                precio: precioo.precio,
+                                desafio: desafio.nombre_desafio,
+                                preoducto: producto.nombre
+                            }
+                            array_productos.push(obj);
+                        })
                     })
-                })
-            });
-            return array_productos;
-        })
-        .then((respuesta)=>{res.send(respuesta)})
-        .catch((err)=>{next(err)});
-        
-                
+                });
+                return array_productos;
+            })
+            .then((respuesta) => { res.send(respuesta) })
+            .catch((err) => { next(err) });
+
+
     } else {
         res.send('pasar un valor de busqueda')
     }
@@ -58,7 +68,7 @@ function productos(req,res,next){
 }
 
 module.exports = {
-productos,
+    productos,
 };
 
 
--- src/midelwares/auth.js
+++ src/midelwares/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const authConfig = require('../../config/auth');
+const { Usuarios } = require('../db');
 
 module.exports = (req, res, next) => {
 
@@ -9,7 +10,6 @@ module.exports = (req, res, next) => {
     if (!req.headers.authorization) {
         res.status(401).json({ msg: "Acceso no autorizado" });// si no existe enviamos mensaje de error
     } else {
-
         // Comrpobar la validez de este token
         let token = req.headers.authorization.split(" ")[1];// el token viene en esta ruta separado por un espacio de otra palabra que no necesitamos
 
@@ -19,8 +19,16 @@ module.exports = (req, res, next) => {
             if (err) {// si hay un error o el token no es valido enviamos un mensaje de error y el error
                 res.status(500).json({ msg: "Ha ocurrido un problema al decodificar el token", err });
             } else {//sino enviamos los datos del usuario por req para su posterior uso
-                req.user = decoded;
-                next();//el next es para que continue 
+                // req.user = decoded.user;
+                // next();//el next es para que continue 
+                //revisar que solo trae un valor en lugar de traer muchos que coincidan.
+                Usuarios.findByPk(decoded.user.id, { 
+                    // where:{ paiseCodigoAlfa: "ARG"}
+                }).then(user => {
+                    //console.log(user.roles);
+                    req.user = user.dataValues;
+                    next();
+                });
             }
 
         })
