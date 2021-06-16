84fb4d4d5759817adf1310eb88c1c801943613b1
nicoarguello <nicoarguello36@gmail.com>
2021-06-16 01:40:19 -0300

    Ruta Admin PUT productos, paises y regiones

--- src/controllers/admin/rutas_put.js
+++ src/controllers/admin/rutas_put.js
@@ -1,4 +1,6 @@
 const {
+  Familia,
+  Productos,
   Paises,
   Regiones,
   Categoria,
@@ -9,13 +11,12 @@ const {
   Generos,
   Clientes,
   Desafios,
-  Familia,
   Subcategoria,
   Tipo_transaccion,
   Transacciones,
-  Productos,
 } = require("../../db");
 
+// ________________ PUT FAMILIA ________________
 async function put_familia(req, res, next) {
   try {
     const { id, nombre_familia, descripcion } = req.body;
@@ -35,14 +36,75 @@ async function put_familia(req, res, next) {
     next(error);
   }
 }
-// await modeloEquipo.update(
-//   {
-//     trabajadoreId: peticion.body.idTrabajador,
-//   },
-//   { where: { id: idEquipo } }
-// );
+
+// ________________ PUT PRODUCTOS ________________
+async function put_productos(req, res, next) {
+  try {
+    const { id, nombre, contenido_neto, unidadMedidaCodigoUnidadMedida, subcategoriumId } = req.body;
+    const productos = await Productos.update(
+      {
+        nombre,
+        contenido_neto,
+        unidadMedidaCodigoUnidadMedida,
+        subcategoriumId
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+      );
+      res.json(productos);
+    } catch (error) {
+    next(error);
+  }
+}
+
+// ________________ PUT PAISES ________________
+async function put_paises(req, res, next) {
+  try {
+    const { codigo_alfa, nombre_pais, regioneId} = req.body;
+    const paises = await Paises.update(
+      {
+        nombre_pais,
+        regioneId,
+        // monedaCodigoMoneda
+      },
+      {
+        where: {
+          codigo_alfa: codigo_alfa,
+        },
+      }
+      );
+      res.json(paises);
+    } catch (error) {
+    next(error);
+  }
+}
+
+// ________________ PUT REGION ________________
+async function put_region(req, res, next) {
+  try {
+    const { id, nombre_region} = req.body;
+    const region = await Regiones.update(
+      {
+        nombre_region,
+      },
+      {
+        where: {
+          id:id
+        },
+      }
+      );
+      res.json(region);
+    } catch (error) {
+    next(error);
+  }
+}
 
 module.exports = {
   put_familia,
-  // categoria,
+  put_productos,
+  put_paises,
+  put_region,
 };
--- src/routes/admin/rutas_admin_put.js
+++ src/routes/admin/rutas_admin_put.js
@@ -1,6 +1,8 @@
 const {
-    pais,
-    region,
+    put_familia,
+    put_productos,
+    put_paises,
+    put_region,
     categoria,
     moneda,
     ciudad,
@@ -9,18 +11,20 @@ const {
     clientes,
     desafio,
     um,
-    put_familia,
     subcategoria,
     tipo_transaccion,
     transacciones,
-    productos
   } = require("../../controllers/admin/rutas_put.js");
   const { Router } = require("express");
   const router = Router();
   
   router.put("/familia", put_familia);
   // Ruta: http://localhost:3001/putadmin/familia
-//   router.get("/categoria", categoria);
-//   // Ruta: http://localhost:3001/getadmin/familia
+  router.put("/productos", put_productos);
+  // Ruta: http://localhost:3001/getadmin/productos
+  router.put("/paises", put_paises);
+  // Ruta: http://localhost:3001/getadmin/paises
+  router.put("/region", put_region);
+  // Ruta: http://localhost:3001/getadmin/region
 
   module.exports = router;
