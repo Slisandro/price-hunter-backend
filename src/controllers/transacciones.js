f30c78ea700130ce4531235f67062923264d89e7
SirCharlex <excelsus.cl@gmail.com>
2021-06-17 17:21:06 -0500

    transacciones de retiro

--- src/controllers/transacciones.js
+++ src/controllers/transacciones.js
@@ -1,5 +1,5 @@
 const {Transacciones, Tipo_transaccion, Usuarios} = require('../db'); //fijarnos el nombre con que lo pone pablo.
-
+const {handlePoints} = require('../../config/funciones_publicas')
 function transacciones (req, res, next){
     const idUsuario = req.params.id
 
@@ -24,6 +24,27 @@ function transacciones (req, res, next){
         console.log(err);
     })
 }
+
+function retiraPuntos (req, res, next){
+    const {usuarioId, puntosRetiro} = req.body
+
+    Usuarios.findOne({
+        where:{
+            id: usuarioId,
+        }
+    }).then(async (datosUser)=>{
+        const metodocobro = datosUser.metodo_de_cobro;
+        const banco = datosUser.banco;
+        const cuenta = datosUser.numero_de_cuenta;
+        const cubreCuenta = '****' + cuenta.slice(cuenta.length - 4, cuenta.length)
+        const observacion = metodocobro + ' ' + banco + ' ' + cubreCuenta;
+        const datosPuntos = await handlePoints(observacion, puntosRetiro, usuarioId, 2);
+
+        return res.send(datosPuntos);
+    })
+}
+
 module.exports = {
     transacciones,
+    retiraPuntos,
 };
