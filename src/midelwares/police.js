ee210c74c67981094e472faf87b43b1edf53c298
PMolina00 <pmolina9876@gmail.com>
2021-06-22 19:40:31 -0300

    politicas

--- src/midelwares/police.js
+++ src/midelwares/police.js
@@ -8,37 +8,56 @@ module.exports = {
      * @param {*} next confirmación y continuación
      */
     restriccion(req, res, next) {
-        if (req.user.tipoUsuarioId === 1 || req.user.tipoUsuarioId === 3) {
-            next()
+        if (req.user) {
+            if (req.user.tipoUsuarioId === 1) {
+                next()
+            } else {
+                res.status(401).send("No estas autorizado");
+            }
+        } else {
+            if (req.admin && req.admin.tipoUsuarioId === 3) {
+                next()
+            } else {
+                res.status(401).send("No estas autorizado");
+            }
+        }
+    },
+    /**
+     * La función para restringir el acceso solo a los usuarios tipo 3(admin)
+     * @param {*} req request con el admin
+     * @param {*} res respuesta en caso de error 
+     * @param {*} next confirmación y continuación
+     */
+    restriccion_admin(req, res, next) {
+        if (req.admin) {
+            if (req.admin.tipoUsuarioId === 3) {
+                next()
+            } else {
+                res.status(401).send("No estas autorizado");
+            }
         } else {
             res.status(401).send("No estas autorizado");
         }
     },
-    // /**
-    //  * La función para restringir el acceso solo a los usuarios tipo 3(admin)
-    //  * @param {*} req request con el admin
-    //  * @param {*} res respuesta en caso de error 
-    //  * @param {*} next confirmación y continuación
-    //  */
-    // restriccion_admin(req, res, next) {
-    //     if (req.user.tipoUsuarioId === 3) {
-    //         next()
-    //     } else {
-    //         res.status(401).send("No estas autorizado");
-    //     }
-    // },
-    // /**
-    //  * La función para restringir el acceso solo a los usuarios tipo 2(cliente) o 3(admin)
-    //  * @param {*} req request con el cliente
-    //  * @param {*} res respuesta en caso de error 
-    //  * @param {*} next confirmación y continuación
-    //  */
-    // restriccion_cliente(req, res, next) {
-    //     if (req.cliente.tipoUsuarioId === 2 || req.user.tipoUsuarioId === 3) {
-    //         next()
-    //     } else {
-    //         res.status(401).send("No estas autorizado");
-    //     }
-    // },
-
+    /**
+     * La función para restringir el acceso solo a los usuarios tipo 2(cliente) o 3(admin)
+     * @param {*} req request con el cliente
+     * @param {*} res respuesta en caso de error 
+     * @param {*} next confirmación y continuación
+     */
+    restriccion_cliente(req, res, next) {
+        if (req.cliente) {
+            if (req.cliente.tipoUsuarioId === 2) {
+                next()
+            } else {
+                res.status(401).send("No estas autorizado");
+            }
+        } else {
+            if (req.admin && req.admin.tipoUsuarioId === 3) {
+                next()
+            } else {
+                res.status(401).send("No estas autorizado");
+            }
+        }
+    },
 }
