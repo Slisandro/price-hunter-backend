8add2fb1a6995a14e7ddfa2e4780f1c068ab6dbc
SirCharlex <excelsus.cl@gmail.com>
2021-07-01 12:11:41 -0500

    correccion ruta de mis desafios... esta es la que funciona

--- src/controllers/detallesdesafios.js
+++ src/controllers/detallesdesafios.js
@@ -7,11 +7,31 @@ const { Op } = require("sequelize");
 
 function DetalleDesafio (req, res, next){
     // const idciudad = req.user.ciudadId
+    // console.log('reqbody',req.body)
     const objGoogle = procesaGoogle(req.body)
     // return res.send(objGoogle)
+    // console.log(objGoogle)
+
+    // [
+    //     {
+    //       "long_name": "Colombia",
+    //       "short_name": "CO",
+    //       "types": [ "country", "political" ]
+    //     },
+    //     {
+    //       "long_name": "Bogotá",
+    //       "short_name": "Bogotá",
+    //       "types": [ "administrative_area_level_1", "political" ]
+    //     },
+    //     {
+    //       "long_name": "Bogotá",
+    //       "short_name": "Bogotá",
+    //       "types": [ "locality", "political" ]
+    //     }
+    //   ]
     
     const [paisFront, nivel1Front, nivel2Front, nivel3Front] = fraccionarApiGoogle(objGoogle)
-    // return res.send(paisFront, nivel1Front, nivel2Front, nivel3Front])
+    // return res.send([paisFront, nivel1Front, nivel2Front, nivel3Front])
        
     Desafios.findAll({
         include:{
@@ -35,12 +55,14 @@ function DetalleDesafio (req, res, next){
             }
         }
     }).then((desafios_detalles)=>{
+        // return res.send(desafios_detalles)
         //Validar que los desafios no hayan caducado para enviarlos al front
         //Validar que la cantidad de precios no exceda la solicitada por el cliente en 
         //cada ciudad.
         if (desafios_detalles.length){
             // return res.json(desafios_detalles)
             const idCiudad = desafios_detalles[0].ciudads[0].id;
+            // console.log('idciudad', idCiudad)
             const desafiosActivos =[];
 
             Precio.findAll({
@@ -54,8 +76,11 @@ function DetalleDesafio (req, res, next){
                 }
     
               }).then((precios)=>{
+                //   console.log(precios)
                 //   return res.json(precios)
-                if (precios.length){
+                // if (precios.length){
+                if (precios){
+
                   for (let x=0; x<desafios_detalles.length;x++){
                       if (fechaContraHoy(desafios_detalles[x].fecha_final)){
                         //observar que precios coinciden con el desafio y la ciudad del precio
@@ -77,8 +102,8 @@ function DetalleDesafio (req, res, next){
                     
                     
                     
-                    return res.send(desafiosActivos);
                 }
+                return res.send(desafiosActivos);
             })
         }else{
             return res.send({msg: 'no hay desafíos cerca a tu ubicación' })
